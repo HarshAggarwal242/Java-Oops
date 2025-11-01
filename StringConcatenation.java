69e0404efa293d73dbe57d3ab809d6e9bcdc097d
Harsh <harshagr1211@gmail.com>
2025-11-01 15:05:18 +0530

    Create StringConcatenation.java

---
+++ StringConcatenation.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class StringConcatenation {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "World";
+        String s3 = s1 + " " + s2; // Using + operator
+        String s4 = s1.concat(" " + s2); // Using concat() method
+        System.out.println(s3); // Hello World
+    }
+}

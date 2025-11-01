038587992a129a240fb55783658c88adff79e3ac
Harsh <harshagr1211@gmail.com>
2025-11-01 15:18:17 +0530

    Create StringSearching.java

---
+++ StringSearching.java
@@ -0,0 +1,9 @@
+class StringSearching {
+    public static void main(String[] args) {
+        String s = "Programming";
+        System.out.println(s.indexOf('g')); 
+        System.out.println(s.lastIndexOf('g')); 
+        System.out.println(s.contains("gram"));  
+    
+    }
+}
